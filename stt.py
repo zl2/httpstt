bef534fc6734d582cc006a0667ce859e1d84b818
zl <zl@home>
2011-09-10 09:37:23 -0400

    Initial release.

---
+++ stt.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import cgi
+import os
+import cgitb
+import sys
+from parseoutput import parse
+
+LIMIT=10
+form = cgi.FieldStorage()
+print "Content-Type: text/html; charset=utf-8\r\n"
+print "\r\n"
+print """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<body>
+"""
+
+def getfile(fieldname,filename):
+    if fieldname not in form:
+        print "Error reading file.<br>"
+        return False
+    fileitem = form[fieldname]
+    if not fileitem.file: 
+        print "Error reading file.<br>"
+        return False
+    f = file(filename, 'wb')
+    size=0
+    while 1:
+        chunk = fileitem.file.read(100000)
+        if not chunk: break
+        f.write(chunk)
+        size+=1
+        if size>LIMIT:
+            print "Error: File too large."
+            return False
+    f.close()
+    return True
+
+#could use a variable filename for archiving
+if "skipget" in form or getfile("file","data/input.wav"):
+    print "File successfully obtained.<br>"
+    sys.stdout.flush()
+    errorcode=os.system("./convertone "+"input.wav")
+
+    parse("output.hyp")
+print """</body>
+</html>
+"""
---
+++ tts.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import cgi
+import os
+import cgitb
+
+form = cgi.FieldStorage()
+print "Content-Type: text/html; charset=utf-8\r\n"
+print "\r\n"
+print """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<body>
+"""
+
+if "text" not in form:
+    print "Error, missing 'text' parameter"
+    sys.exit(0)
+else:
+    text=form["text"].value
+if "voice" not in form:
+    voice="en"
+else:
+    voice=form["voice"].value
+if "speed" is not form:
+    speed="110"
+else:
+    speed=form["speed"].value
+
+#print 'espeak -w data/out.wav -v '+voice+' -s '+speed+' "'+text+'"'
+os.system('espeak -w data/out.wav -v '+voice+' -s '+speed+' "'+text+'"')
+os.system('lame data/out.wav data/out.mp3')
+print '<a href="data/out.mp3">Answer</a>
+print """</body>
+</html>
+"""
